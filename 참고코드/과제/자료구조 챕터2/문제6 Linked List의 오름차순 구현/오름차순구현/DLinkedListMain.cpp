d47daeb0a76e8e611c662d0bc44045904fc7274e
lee-do-hun <tkdgus4977@naver.com>
2020-03-11 14:56:51 +0900

    3/11 working

---
+++ 참고코드/과제/자료구조 챕터2/문제6 Linked List의 오름차순 구현/오름차순구현/DLinkedListMain.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "DLinkedList.h"
+#include "Point.h"
+
+int main(void)
+{
+	// List의 생성 및 초기화
+	List list;
+	Point *pp;
+	Point *compare;
+	ListInit(&list);
+
+	// 비교 (x가 2거나 y가 2면 삭제할 예정)
+	compare = new Point;
+	compare->xpos = 2;
+	compare->ypos = 2;
+
+	// 5개의 데이터 저장
+
+	for (int i = 0, j = 1; i < 5; i++)
+	{
+		pp = new Point;
+		SetPointPos(pp, j, i);
+		Insert(&list, pp);
+		j++;
+	}
+
+
+	// 저장된 데이터의 전체 출력 /////////////////////////
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &pp))    // 첫 번째 데이터 조회
+	{
+		printf("%d %d\n", pp->xpos, pp->ypos);
+
+		while (LNext(&list, &pp))    // 두 번째 이후의 데이터 조회
+			printf("%d %d\n", pp->xpos, pp->ypos);
+	}
+	printf("\n\n");
+
+	// 숫자 2를 검색하여 모두 삭제 //////////////////////////
+
+	if (LFirst(&list, &pp)) // TRUE가 반환되면
+	{
+		if (PointComp(pp, compare) == 1 || PointComp(pp, compare) == 2)
+			LRemove(&list);
+
+		while (LNext(&list, &pp))
+		{
+			if (PointComp(pp, compare) == 1 || PointComp(pp, compare) == 2)
+				LRemove(&list);
+		}
+	}
+
+	// 삭제 후 남아있는 데이터 전체 출력 ////////////////////////
+	printf("현재 데이터의 수: %d \n", LCount(&list));
+
+	if (LFirst(&list, &pp))
+	{
+		printf("%d %d\n", pp->xpos, pp->ypos);
+
+		while (LNext(&list, &pp))
+			printf("%d %d\n", pp->xpos, pp->ypos);
+	}
+	printf("\n\n");
+
+	system("pause");
+
+	return 0;
+}
