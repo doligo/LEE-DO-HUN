1356f8af0dfad51127e74f7b0ca405870919359e
LEE-DO-HUN <tkdgus4977@naver.com>
2020-04-17 15:05:00 +0900

    study

--- 참고코드/과제/자료구조 챕터6/테이블해쉬/Project7777777/Project7777777/main.cpp
+++ 참고코드/과제/자료구조 챕터6/테이블해쉬/Project7777777/Project7777777/main.cpp
@@ -1,11 +1,16 @@
 ﻿#include "Table.h"
 
+int GetHashValue(int hf)
+{
+	return hf % 100;
+}
+
 int main()
 {
 	Table TB;
 	TableS tbs;
 
-	TB.TBLInit(&tbs, );
+	TB.TBLInit(&tbs, GetHashValue());
 
 	system("pause");
 }
