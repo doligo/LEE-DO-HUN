f6cd0f0a4ee7d98498cf4f1deef88673261955d3
Lee Do Hun <tkdgus4977@naver.com>
2020-05-24 22:41:32 +0900

    not yet

---
+++ 참고코드/과제/학생관리_싱글톤구현/학생관리/Std_Manager.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "Student.h"
+
+#define S_MAX 30
+
+class Std_manager 
+{
+	Student* std[S_MAX];
+
+	static Std_manager *m_stmanager;
+public:
+	static Std_manager * GetInstance()
+	{
+		if (m_stmanager == NULL)
+			m_stmanager = new Std_manager;
+
+		return m_stmanager;
+	}
+
+	Std_manager();
+
+	void display();
+	void setStudent();
+	void showStudent();
+	void findNumber();
+	void findname();
+	void findClass();
+
+	~Std_manager()
+	{
+		for (int i = 0; i < 30; i++)
+		{
+			delete std[i];
+		}
+	}
+
+};
+
---
+++ 참고코드/과제/학생관리_싱글톤구현/학생관리/Student.h
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#define max_len 100
+using namespace std;
+
+template <typename> // ÅÛÇÃ¸´Àû¿ë
+class Student
+{
+private:
+	string name;
+	int kor, eng, math, sum;
+	float avg;
+	int num;
+	char Class;
+public:
+	Student() { num = 0; }
+	~Student() {}
+
+	void SetStudent(int _num);
+	void showStudent();
+	int getnum()
+	{
+		return num;
+	}
+	string getname()
+	{
+		return name;
+	}
+	char getClass()
+	{
+		return Class;
+	}
+};
+
