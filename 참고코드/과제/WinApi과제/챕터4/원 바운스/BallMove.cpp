13634dd26b9f2104ff710fa4a623e07db795efc9
LEE-DO-HUN <tkdgus4977@naver.com>
2020-07-03 14:57:54 +0900

    not yet s

---
+++ 참고코드/과제/WinApi과제/챕터4/원 바운스/BallMove.cpp
@@ -0,0 +1,122 @@
+#include<windows.h>
+#include<time.h>
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("BallMove");
+void DrawCircle(HDC hdc, int x, int y);
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime);
+
+#define RIGHT_UP 10
+#define RIGHT_DOWN 11
+#define LEFT_UP 12
+#define LEFT_DOWN 13
+
+int x = 0;
+int y = 0;
+RECT rect;
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPervlnstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	srand((unsigned)time(NULL));
+	x = (rand() % 800);
+	y = (rand() % 500);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while(GetMessage(&Message, NULL, 0, 0))
+	{
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return (int)Message.wParam;
+}
+
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	SYSTEMTIME st;
+	static TCHAR sTime[128];
+
+	GetClientRect(hWnd, &rect);
+
+	switch(iMessage)
+	{
+		case WM_CREATE:
+			SetTimer(hWnd, 1, 100, TimeProc);
+			//SendMessage(hWnd, WM_TIMER, 1, 0);
+			return 0;
+		case WM_PAINT:
+			hdc = BeginPaint(hWnd, &ps);
+			Ellipse(hdc, x, y, x + 50, y + 50);
+			EndPaint(hWnd, &ps);
+			return 0;
+		case WM_DESTROY:
+			KillTimer(hWnd, 1);
+			PostQuitMessage(0);
+			return 0;
+	}
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+void CALLBACK TimeProc(HWND hWnd, UINT uMsg, UINT idEvent, DWORD dwTime)
+{
+	int my_direct = 0;
+
+	my_direct = rand() % 4 + 10;
+
+	if (my_direct == RIGHT_UP)
+	{
+		while (x + 50 <= rect.right || y - 50 >= rect.top)
+		{
+			x += 10;
+			y -= 10;
+		}
+	}
+	else if (my_direct == RIGHT_DOWN)
+	{
+		while (x + 50 <= rect.right || y + 50 <= rect.bottom)
+		{
+			x += 10;
+			y += 10;
+		}
+	}
+	else if (my_direct == LEFT_UP)
+	{
+		while (x - 50 >= rect.left || y - 50 >= rect.top)
+		{
+			x -= 10;
+			y -= 10;
+		}
+	}
+	else if (my_direct == LEFT_DOWN)
+	{
+		while (x - 50 >= rect.left || y + 50 <= rect.bottom)
+		{
+			x -= 10;
+			y += 10;
+		}
+	}
+
+	InvalidateRect(hWnd, NULL, TRUE);
+}
