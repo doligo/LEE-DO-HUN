7ea6f538e7adc581616ab0544e3eb2a76b9bdfde
Lee Do Hun <tkdgus4977@naver.com>
2020-11-04 21:22:09 +0900

    플로차트v2, 배틀시티 시작

---
+++ 참고코드/과제/WinApi과제/챕터10/BattleCity/Project8/Project8/BitMap_And_Draw.cpp
@@ -0,0 +1,11 @@
+#include "BitMap_And_Draw.h"
+
+BitMap_And_Draw::BitMap_And_Draw()
+{
+
+}
+
+BitMap_And_Draw::~BitMap_And_Draw()
+{
+
+}
---
+++ 참고코드/과제/WinApi과제/챕터10/BattleCity/Project8/Project8/BitMap_And_Draw.h
@@ -0,0 +1,12 @@
+#pragma once
+class BitMap_And_Draw
+{
+private:
+public:
+	BitMap_And_Draw();
+
+
+
+	~BitMap_And_Draw();
+};
+
