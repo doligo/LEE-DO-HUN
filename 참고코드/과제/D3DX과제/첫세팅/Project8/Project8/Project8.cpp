ee8853f4bd9c1bb0caaf5ba2f608da492197ca44
doligo <tkdgus4977@naver.com>
2021-04-07 14:54:52 +0900

    d3d

---
+++ 참고코드/과제/D3DX과제/첫세팅/Project8/Project8/Project8.cpp
@@ -0,0 +1,97 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <d3d9.h>
+#include <vector>
+using namespace std;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPDIRECT3D9 g_pD3D = NULL;
+LPDIRECT3DDEVICE9 g_pD3DDevice = NULL;
+
+char g_szClassName[256] = "D3D Init";
+#pragma comment(lib, "msimg32.lib") // 링커 2019 오류 뜨면 추가해주면 된다.
+
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hInstance = hInstance;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.lpszClassName = g_szClassName;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(NULL), NULL, NULL, NULL, NULL, g_szClassName, NULL };
+
+	RegisterClassEx(&wc); // 레지스터에 등록하기
+
+	hWnd = CreateWindow(g_szClassName, g_szClassName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+		1200, 738, NULL, (HMENU)NULL, hInstance, NULL);
+
+	//if (SUCCEEDED())
+
+	ShowWindow(hWnd, nCmdShow);
+
+	while (1)
+	{
+		if (PeekMessage(&Message, NULL, 0, 0, PM_REMOVE))
+		{
+			if (Message.message == WM_QUIT)
+				break;
+
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);
+		}
+		else
+		{
+		}
+
+	}
+
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc = NULL;
+	PAINTSTRUCT ps;
+
+	switch (iMessage)
+	{
+	case WM_CREATE:
+		return 0;
+	case WM_KEYDOWN:
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		EndPaint(hWnd, &ps);
+		return 0;
+	case WM_TIMER:
+		return 0;
+	case WM_DESTROY:
+
+		PostQuitMessage(0);
+
+		return 0;
+	}
+
+	return(DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
+
+HRESULT InitD3D(HWND hWnd)
+{
+	//if (NULL == )
+}
