63fea3bc698220839863c79e65daa16e9f85cd9d
Lee Do Hun <tkdgus4977@naver.com>
2021-04-09 00:25:30 +0900

    123

--- 참고코드/과제/D3DX과제/첫세팅/Project8/Project8/Project8.cpp
+++ 참고코드/과제/D3DX과제/첫세팅/Project8/Project8/Project8.cpp
@@ -1,10 +1,14 @@
 ﻿#include <Windows.h>
 #include <iostream>
 #include <d3d9.h>
+#include <d3d9types.h>
 #include <vector>
 using namespace std;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HRESULT InitD3D(HWND hWnd);
+void Render();
+
 HINSTANCE g_hInst;
 LPDIRECT3D9 g_pD3D = NULL;
 LPDIRECT3DDEVICE9 g_pD3DDevice = NULL;
@@ -15,7 +19,6 @@ char g_szClassName[256] = "D3D Init";
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
 {
-
 	HWND hWnd;
 	MSG Message;
 	WNDCLASS WndClass;
@@ -40,8 +43,27 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	hWnd = CreateWindow(g_szClassName, g_szClassName, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
 		1200, 738, NULL, (HMENU)NULL, hInstance, NULL);
 
-	//if (SUCCEEDED())
+	if (SUCCEEDED(InitD3D(hWnd))) // 초기화 성공시
+	{
+		ShowWindow(hWnd, SW_SHOWDEFAULT);
+		UpdateWindow(hWnd);
 
+		MSG msg;
+
+		while (GetMessage(&msg, NULL, 0, 0))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	else
+	{
+		//등록된 레지스트 winclass 릴리즈
+		UnregisterClass("D3D Init", wc.hInstance);
+		return 0;
+	}
+
+	/*
 	ShowWindow(hWnd, nCmdShow);
 
 	while (1)
@@ -61,6 +83,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	}
 
 	return (int)Message.wParam;
+	*/
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
@@ -93,5 +116,42 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam)
 
 HRESULT InitD3D(HWND hWnd)
 {
-	//if (NULL == )
+	if (NULL == (g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
+		return E_FAIL;
+
+	D3DPRESENT_PARAMETERS d3dpp;
+	ZeroMemory(&d3dpp, sizeof(d3dpp));
+
+	d3dpp.Windowed = TRUE;
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+
+	// 디바이스를 설정해서 생성
+	// 디폴트 비디오카드를 사용하고, HAL 디바이스를 생성한다.
+	// 정점 처리는 모든 카드에서 지원하는 SW처리로 생성한다.
+	if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pD3DDevice)))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+void Render()
+{
+	if (NULL == g_pD3DDevice)
+		return;
+
+	// 후면 버퍼를 파란색(0, 0, 255)으로 지운다.
+	g_pD3DDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
+
+	if (SUCCEEDED(g_pD3DDevice->BeginScene()))
+	{
+		//실제 렌더링 명령들이 나열될 곳
+
+
+		//렌더링 종료
+		g_pD3DDevice->EndScene();
+	}
+
+	// 후면 버퍼를 보이는 화면으로 전환.
+	g_pD3DDevice->Present(NULL, NULL, NULL, NULL);
 }
